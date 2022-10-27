163ca0bf64673b2ecd30dfd0d209045c92381b41
Lorenzo De Marchi <LorenzoDeMarchi9@gmail.com>
2022-10-27 01:02:03 -0300

    e

--- js/MusicApp.js
+++ js/MusicApp.js
@@ -1,14 +1,50 @@
 var currentImgIndex= 1;
-var ImgArray = [
-'chat-left-heart-fill.svg',
-'chat-left-heart-fill-red.png'
+var ImgArray1 = [
+'svg/chat-left-heart-fill.svg',
+'png/chat-left-heart-fill-red.png'
 ];
 
-function changeLike(){
-    console.log("gfonikjflsdf");
-    if(currentImgIndex == ImgArray.length){
+var ImgArray2 = [
+    'svg/chat-left-heart-fill.svg',
+    'png/chat-left-heart-fill-red.png'
+];
+
+var ImgArray3 = [
+    'svg/chat-left-heart-fill.svg',
+    'png/chat-left-heart-fill-red.png'
+];
+
+var ImgArray4 = [
+    'svg/chat-left-heart-fill.svg',
+    'png/chat-left-heart-fill-red.png'
+];
+
+
+function changeLike1(){
+    if(currentImgIndex == ImgArray1.length){
         currentImgIndex = 0
     }
-    document.getElementById("like").src=ImgArray[currentImgIndex];
+    document.getElementById("like1").src=ImgArray1[currentImgIndex];
         currentImgIndex++;
 }   
+function changeLike2(){
+    if(currentImgIndex == ImgArray2.length){
+        currentImgIndex = 0
+    }
+    document.getElementById("like2").src=ImgArray2[currentImgIndex];
+        currentImgIndex++;
+}   
+function changeLike3(){
+    if(currentImgIndex == ImgArray3.length){
+        currentImgIndex = 0
+    }
+    document.getElementById("like3").src=ImgArray3[currentImgIndex];
+        currentImgIndex++;
+}  
+function changeLike4(){
+    if(currentImgIndex == ImgArray4.length){
+        currentImgIndex = 0
+    }
+    document.getElementById("like4").src=ImgArray4[currentImgIndex];
+        currentImgIndex++;
+}  
